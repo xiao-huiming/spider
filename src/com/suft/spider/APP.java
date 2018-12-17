d606d51b33e81a9aba43a3720793631756745dc8
xiao-huiming <1551995337@qq.com>
2018-12-17 19:05:14 +0800

    课程设计
    爬虫使用初解

--- src/com/suft/spider/APP.java
+++ src/com/suft/spider/APP.java
@@ -1,5 +1,59 @@
-package com.suft.spider;
+package edu.csuft.wtao.spider;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * 
+ * @author 
+ *
+ */
 public class APP {
 
+	// alt + /
+	// ctrl + s
+	public static void main(String[] args) {
+
+		// 目标 URL
+		String url = "https://movie.douban.com/top250";
+
+		// 使用 JSOUP 抓去文档
+		try {
+			Document doc = Jsoup.connect(url).get();
+
+			Elements es = doc.select(".grid_view .item");
+			System.out.println(es.size());
+
+			// 创建一个影片的列表
+			ArrayList<FILM> list = new ArrayList<>();
+
+			for (Element e : es) {
+				FILM f = new FILM();
+				// 每一部影片
+				Element t = e.select(".title").first();
+				String num = e.select(".star span").last().text();
+				System.out.println(t.text() + ", " + num);
+
+//				f.id
+//				f.title
+				list.add(f);
+			}
+
+//			String title = doc.title();
+//			String data = doc.data();
+
+//			System.out.println(title);
+//			System.out.println(data);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
---
+++ src/com/suft/spider/FILM.java
@@ -0,0 +1,46 @@
+package com.suft.spider;
+
+/**
+ * 影片
+ * 
+ * @author 
+ *
+ */
+public class FILM {
+
+	/**
+	 * 影片名称
+	 */
+	String title;
+
+	/**
+	 * 相关信息
+	 */
+	String info;
+
+	/**
+	 * 评分
+	 */
+	double rating;
+
+	/**
+	 * 评分人数
+	 */
+	int num;
+
+	/**
+	 * 排名
+	 */
+	int id;
+
+	/**
+	 * 海报
+	 */
+	String poster;
+
+	/**
+	 * 短评
+	 */
+	String quote;
+
+}
